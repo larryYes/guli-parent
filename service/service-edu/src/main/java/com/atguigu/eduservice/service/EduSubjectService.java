7bf9f3d8061e47e5e119e67587fe8bb43cc97d02
lgj <liu9801@foxmail.com>
2020-12-25 11:17:59 +0800

    生成课程分类表相关代码

---
+++ service/service-edu/src/main/java/com/atguigu/eduservice/service/EduSubjectService.java
@@ -0,0 +1,16 @@
+package com.atguigu.eduservice.service;
+
+import com.atguigu.eduservice.entity.EduSubject;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 课程科目 服务类
+ * </p>
+ *
+ * @author liugj
+ * @since 2020-12-25
+ */
+public interface EduSubjectService extends IService<EduSubject> {
+
+}
